1503a1c26f42b7aacc84ea00cf6ec0e701d765dd
yanalshmilan <yshmilan@gmail.com>
2022-02-06 14:56:37 +0200

    fix

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 });
 
 // Routes
-app.use('/products', productsRoutes);
+app.use('api/products', productsRoutes);
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({
